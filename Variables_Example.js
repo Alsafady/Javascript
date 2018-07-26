13b0cf827c0e7717dd46ced5325547928383e7ab
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2018-07-26 23:03:33 +0300

    Variables

---
+++ Variables_Example.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 7;
+var sum = a + b;
+
+
+
+var myNumber = 3;                   // a number
+var myString = "Hello, World!"      // a string
+var myBoolean = true;               // a boolean
